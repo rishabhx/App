603b8e3317cb1388e4d37402eb19d34aafe9ca50
rishabhx <b.rishabh2004@gmail.com>
2017-03-11 10:21:59 -0500

    Chapter 4
    Chapter 4

---
+++ Rishabh/Foodie/Foodie/ViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  FooTracker
+//
+//  Created by Rishabh Bhandari on 2/10/17.
+//  Copyright © 2017 Rishabh Bhandari. All rights reserved.
+//
+
+import UIKit
+import os.log
+
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        mealNameLabel.text = textField.text
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+
+    //MARK: Actions
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        // Hide the keyboard.
+        nameTextField.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .photoLibrary
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+        
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
+    
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+        
+
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    
+    }
+    
+
+}
+
---
+++ Rishabh/Foodie/Rating Control.swift
@@ -0,0 +1,19 @@
+//
+//  Rating Control.swift
+//  Foodie
+//
+//  Created by Rishabh Bhandari on 3/6/17.
+//  Copyright © 2017 Rishabh Bhandari. All rights reserved.
+//
+
+import UIKit
+
+class Rating_Control: UIStackView {
+    //MARK: Initialization
+    override init(frame: CGRect) {
+       super.init(frame: frame)
+    }
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
